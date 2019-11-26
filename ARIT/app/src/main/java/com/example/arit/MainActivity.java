fb4688645acf2a493dcac7bdd6d199a45a2465b8
sonhamin <33140181+sonhamin@users.noreply.github.com>
2019-11-26 11:33:20 +0900

    Firebase Updated

--- ARIT/app/src/main/java/com/example/arit/MainActivity.java
+++ ARIT/app/src/main/java/com/example/arit/MainActivity.java
@@ -1,14 +1,127 @@
 package com.example.arit;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText idEdit, passEdit;
+    Button loginButton, signupButton;
+    String userId, userPass;
+    private DatabaseReference uDatabase;
+    boolean check = false;
+    SharedPreferences loginPref;
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.7F);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idEdit = (EditText) findViewById(R.id.idEdit);
+        passEdit = (EditText) findViewById(R.id.passEdit);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signupButton = (Button) findViewById(R.id.signupButton);
+        uDatabase = FirebaseDatabase.getInstance().getReference("user");
+        loginPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        final Intent intent = new Intent(MainActivity.this, Home.class);
+        final Intent intent2 = new Intent(MainActivity.this, SignIn.class);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //v.startAnimation(buttonClick);
+                if (idEdit.getText() != null && passEdit.getText() != null) {
+                    userId = idEdit.getText().toString();
+                    userPass = passEdit.getText().toString();
+                    getUserDatabase();
+                    if (check == true) {
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Please check your id and password or try again", Toast.LENGTH_SHORT).show();
+                    }
+                    check = false;
+                } else {
+                    Toast.makeText(MainActivity.this, "Please check your id and password or try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(intent2);
+            }
+        });
+
+
     }
+    public void getUserDatabase() {
+        final ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    GenericTypeIndicator<Map<String, Object>> genericTypeIndicator = new GenericTypeIndicator<Map<String, Object>>() {};
+                    Map<String, Object> map = snapshot.child("").getValue(genericTypeIndicator);
+
+                    String tempStorage = map.toString();
+                    String temp2Storage = tempStorage.replaceAll("[{}]", "");
+                    String tempArray[] = temp2Storage.split(",");
+
+                    String PW = "", ID = "";
+
+
+                    for(int i = 0; i < 4; i++)
+                    {
+                        if(tempArray[i].charAt(0) == 'p'){                                             PW = tempArray[i].substring(3);                        }
+                        else if((tempArray[i].charAt(0) == ' ' && tempArray[i].charAt(1) == 'p')){       PW = tempArray[i].substring(4);                      }
+
+                        else if(tempArray[i].charAt(0) == 'i'){                                        ID = tempArray[i].substring(3);                        }
+                        else if( (tempArray[i].charAt(0) == ' ' && tempArray[i].charAt(1) == 'i')){   ID = tempArray[i].substring(4);                         }
+                    }
+
+
+                    Log.e("TAG", "zero: " + PW + " one: " + ID);
+
+
+                    if (userId.equals(ID) && userPass.equals(PW)) {
+                        check = true;
+                        break;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.v("loger", "faile to read value");
+                Log.v("loger", databaseError.toString());
+            }
+        };
+        uDatabase.addValueEventListener(postListener);
+    }
+
+
 }
--- ARIT/app/src/main/java/com/example/arit/SignIn.java
+++ ARIT/app/src/main/java/com/example/arit/SignIn.java
@@ -2,13 +2,158 @@ package com.example.arit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignIn extends AppCompatActivity {
+
+    EditText idEdit, passEdit, nameEdit, phoneEdit;
+    Button singupButton, cancelButton;
+    private DatabaseReference uDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        idEdit = findViewById(R.id.idEdit2);
+        passEdit = findViewById(R.id.passEdit2);
+        nameEdit = findViewById(R.id.nameEdit);
+        phoneEdit = findViewById(R.id.phoneEdit);
+
+        singupButton = findViewById(R.id.signupButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        uDatabase = FirebaseDatabase.getInstance().getReference("user");
+
+
+
+        final Intent intent = new Intent(SignIn.this, MainActivity.class);
+        final Intent intent2 = new Intent(SignIn.this, Home.class);
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
+        singupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int check = checkSignup();
+                if(check == 5){
+                    Toast.makeText(SignIn.this, "Correct", Toast.LENGTH_SHORT).show();
+
+                    postUserInfo(idEdit.getText().toString(), passEdit.getText().toString(), nameEdit.getText().toString(), phoneEdit.getText().toString());
+                    startActivity(intent);
+
+
+                }
+                else{
+                    if(check == 0){  Toast.makeText(SignIn.this, "Please check ID and enter again", Toast.LENGTH_SHORT).show();    }
+                    if(check == 1){  Toast.makeText(SignIn.this, "Please check PW and enter again", Toast.LENGTH_SHORT).show();    }
+                    if(check == 2){  Toast.makeText(SignIn.this, "Please check Name and enter again", Toast.LENGTH_SHORT).show();    }
+                    if(check == 3){  Toast.makeText(SignIn.this, "Please check Phone Number and enter again", Toast.LENGTH_SHORT).show();    }
+
+                }
+
+
+
+            }
+        });
+
+
+
+
+
+
     }
+
+    private int checkSignup() {
+
+        int id = 0, pass = 0, name = 0, phone = 0;
+        Log.e("tag", "WOT: " + idEdit.length() );
+
+        if(idEdit.length() >= 2)
+        {
+            id++;
+        }
+        if(passEdit.length() >= 2)
+        {
+            pass++;
+        }
+        if(nameEdit.length() >= 2)
+        {
+            name++;
+        }
+        if(phoneEdit.length() >= 2)
+        {
+            phone++;
+        }
+
+
+        if(id > 0 && pass > 0 && name > 0 && phone > 0)
+        {
+            return 5;
+        }
+
+        else{
+
+            if(id == 0){                return 0;            }
+            else if(pass == 0){         return 1;             }
+            else if(name == 0){         return 2;            }
+            else if(phone == 0){        return 3;            }
+        }
+
+        return 0;
+    }
+
+
+    public void postUserInfo(String one, String two, String three, String four) {
+
+        Map<String, Object> userUpdates = new HashMap<>();
+        Map<String, Object> postValues = new HashMap<>();
+
+        String User = one;
+        String Password = two;
+        String Name = three;
+        String PhoneNum = four;
+
+        String All = "{"+User+","+Password+","+Name+","+PhoneNum+"}";
+        Log.e("tag", "WOTTT: "+All);
+
+        postValues.put("id", User);
+        postValues.put("pw", Password);
+        postValues.put("name", Name);
+        postValues.put("numphone", PhoneNum);
+
+
+
+
+        userUpdates.put("/"+User, postValues);
+        uDatabase.updateChildren(userUpdates);
+        Log.e("tag", "WORK: "+userUpdates.toString());
+        idEdit.setText("");
+        passEdit.setText("");
+        nameEdit.setText("");
+        phoneEdit.setText("");
+    }
+
+
+
+
+
 }
