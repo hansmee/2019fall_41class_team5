d7a7c16d392fc1698198d58231b13916f266ef9a
hyunmook-lim <53961996+hyunmook-lim@users.noreply.github.com>
2019-12-01 18:57:44 +0900

    change fragment by buttons

---
+++ ARIT/app/src/main/java/com/example/arit/FrameLayout.java
@@ -0,0 +1,90 @@
+package com.example.arit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class FrameLayout extends AppCompatActivity implements View.OnClickListener{
+
+    private final int Frag1 = 1;
+    private final int Frag2 = 2;
+    private final int Frag3 = 3;
+    private final int Frag4 = 4;
+
+    Button btn_tab1, btn_tab2, btn_tab3, btn_tab4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+
+        btn_tab1 = (Button)findViewById(R.id.homebtn);
+        btn_tab2 = (Button)findViewById(R.id.categorybtn);
+        btn_tab3 = (Button)findViewById(R.id.searchbtn);
+        btn_tab4 = (Button)findViewById(R.id.mypagebtn);
+
+        btn_tab1.setOnClickListener(this);
+        btn_tab2.setOnClickListener(this);
+        btn_tab3.setOnClickListener(this);
+        btn_tab4.setOnClickListener(this);
+
+        callFragment(Frag1);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.homebtn :
+                callFragment(Frag1);
+                break;
+            case R.id.categorybtn :
+                callFragment(Frag2);
+                break;
+            case R.id.searchbtn :
+                callFragment(Frag3);
+                break;
+            case R.id.mypagebtn :
+                callFragment(Frag4);
+                break;
+        }
+    }
+
+    private void callFragment(int fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (fragment) {
+            case 1:
+
+                HomeFrag homeFrag = new HomeFrag();
+                transaction.replace(R.id.fragmentContainer, homeFrag);
+                transaction.commit();
+                break;
+
+            case 2 :
+
+                CategoryFrag categoryFrag = new CategoryFrag();
+                transaction.replace(R.id.fragmentContainer, categoryFrag);
+                transaction.commit();
+                break;
+
+            case 3 :
+
+                SearchFrag searchFrag = new SearchFrag();
+                transaction.replace(R.id.fragmentContainer, searchFrag);
+                transaction.commit();
+                break;
+
+            case 4 :
+
+                MypageFrag mypageFrag = new MypageFrag();
+                transaction.replace(R.id.fragmentContainer, mypageFrag);
+                transaction.commit();
+                break;
+
+        }
+    }
+}
---
+++ ARIT/app/src/main/java/com/example/arit/MypageFrag.java
@@ -0,0 +1,28 @@
+package com.example.arit;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MypageFrag extends Fragment {
+
+    public MypageFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage, container, false);
+    }
+
+}
