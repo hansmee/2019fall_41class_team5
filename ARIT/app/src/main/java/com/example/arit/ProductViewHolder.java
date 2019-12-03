70274898b7f7938e9e221f10815af9b6f748204e
HanSangMee <ludia1234@naver.com>
2019-12-03 14:40:30 +0900

    (EDIT_FILE)\nPost.java  -> add post function\nHome.java -> go to post + recent products list\nMainActivity.java -> intent user information\nactivity_post.xml \nactivity_home.xml\nmanifest.xml -> add picture permission \ngradle (Module:app) -> add firebase-storage, Glide\n(NEW_FILE)\nProductItem.java\nProductAdapter.java\nProductViewHolder.java\nProductDetail.java -> add detailed products information\nactivity_product_detail.xml\nlist_item.xml

---
+++ ARIT/app/src/main/java/com/example/arit/ProductViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.arit;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProductViewHolder {
+    public TextView tv1;    // 카테고리
+    public TextView tv2;    // 제목
+    public TextView tv3;    // 제품명
+    public TextView tv4;    // 가격
+    public TextView tv5;    // 거래방식
+    public ImageView iv1;   // 이미지
+}
